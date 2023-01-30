e939ad57f2730fa3991992a9564df28dd46518a1
Scipiia <kuznetcov.artem.va@gmail.com>
2023-01-30 09:27:15 +0800

    folder models and created struct  model DB, add snippets.go with 3 sql methods (insert, get, latest)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"Scipiia/snippetbox.git/pkg/models/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
+	dsn := flag.String("dsn", "web:simplepass@/snippetbox?parseTime=true", "подключение к Mysql")
 	flag.Parse()
 
 	//logger
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	//DB
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	//init struct
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	//struct server
@@ -36,11 +51,23 @@ func main() {
 	}
 
 	infoLog.Printf("start server on port %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
 
-//защищает досптуп к статическим файлам
+//DB
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+//защищает доступ к статическим файлам
 type neuteredFileSystem struct {
 	fs http.FileSystem
 }
